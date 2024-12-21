75310c626684e4162b00cfb1e5c6ecd5a0755bcb
Stanislav <royultra77@gmail.com>
2024-12-21 11:25:00 +0300

    fix size img

--- src/components/ShowFullItem.jsx
+++ src/components/ShowFullItem.jsx
@@ -1,16 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class ShowFullItem extends Component {
     render() {
         return (
-            <div className="full-item">
+            <div className="full-item" onClick={(e) => {
+                // Закрываем карточку при клике на фон
+                if (e.target.classList.contains('full-item')) {
+                    this.props.onShowItem(null);
+                }
+            }}>
                 <div>
-                    <img src={'./image/goods/' + this.props.item.img}
-                         onClick={() => this.props.onShowItem(this.props.item)}/>
+                    {/* Кнопка для закрытия карточки */}
+                    <button className="close-btn" onClick={() => this.props.onShowItem(null)}>×</button>
+                    <img src={`/react-proSport-app/${this.props.item.img}`} alt={this.props.item.title} />
+
                     <h2>{this.props.item.title}</h2>
                     <p>{this.props.item.description}</p>
                     <b>{this.props.item.price}₽</b>
-                    <div className='add-to-cart' onClick={() => {this.props.onAdd(this.props.item)}}>+</div>
+                    <div className="add-to-cart" onClick={() => { this.props.onAdd(this.props.item) }}>+</div>
                 </div>
             </div>
         );
